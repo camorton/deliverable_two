33a9655ec72ac36a22bfcf03aa4c4d9137da09df
chris <camorton2000@yahoo.com>
2020-09-08 16:36:48 -0400

    First commit for deliverable two

---
+++ js/script.js
@@ -0,0 +1,11 @@
+<script>
+        var rain = prompt ("How many inches of rain fell?")
+        var fertilizer = prompt (Did you use fertilizer?)
+        var premorreg = prompt (Did you use premium or regular?)
+
+    function {
+        "*".repeat(rain)
+    console.log();
+
+    
+</script>
