9076eb98c01ed8458a41d9cef1d3d4e998cf90c9
chris <camorton2000@yahoo.com>
2020-09-14 21:18:45 -0400

    feedbackFixes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,27 @@
 
-
 let rainfall = prompt("How many inches of rain fell?");
 let rainfallInches = "*";
 console.log("Rain:" + rainfallInches.repeat(rainfall));
 
-if (rainfallInches < 10);
-var originalYield = (50 * .8);    
+let originalYield=50; 
+let answer = prompt ("Did you use fertilizer?");
+let brand = prompt ("Did you use premium or regular?")
 
-if ((rainfallInches > 10 && rainfallInches < 19));
-    var originalYield = 50;
 
-if (rainfallInches >= 20);
-    var originalYield = (50 * .9);
+if (rainfall < 10){
+    originalYield = (50 * .8);    
+}
 
-let fertilizer = prompt("Did you use fertilizer?");
+if (rainfall >= 20) {
+    originalYield = (50 * .9);
+}
 
-if (fertilizer==="yes") {
-    var brand = prompt("Did you use premium or regular?");
-} else {
-    console.log("The yield should be " + originalYield + " bushels per acre") 
-} 
-    
-var finalYield = (originalYield * 1.15);
-if (brand==="premium");
-    console.log("The yield should be " + finalYield + " bushels per acre")
-
-var finalYield = (originalYield * 1.1)
-if (brand==="regular")
-    console.log("The yield should be " + finalYield + " bushels per acre")
+if (answer === "no") {
+    console.log ("The yield should be " + originalYield + " bushels per acre");
+}   else if (brand === "premium") {
+    console.log ("The yield should be " + (originalYield * 1.15) + " bushels per acre");
+}   else {
+    console.log ("The yield should be " + (originalYield * 1.1) + " bushels per acre");
+}
 
 
